a71df6ee23dc6c46b85639122e7c7eb558a4cd1c
Allan Branson <bransonaallan@gmail.com>
2024-05-15 22:52:17 +0300

    Creating a fileReader and file directory addres to open a file on the text area.

--- Function_File.java
+++ Function_File.java
@@ -1,9 +1,18 @@
 import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Function_File {
     GUI gui;
+    String fileName;
+    String fileAddress;
+
+
     public Function_File ( GUI gui) {
         this.gui = gui;
+       
 
     }
 
@@ -13,9 +22,33 @@ public class Function_File {
         gui.window.setTitle("New");
     }
 
+
+    //Method to open  a file dialog, 
+    //display the file name on the title name 
+    //get the directory of the file
     public void open () {
 
         FileDialog fileDialog = new FileDialog(gui.window, "Open", FileDialog.LOAD);
         fileDialog.setVisible(true);
+
+        if (fileDialog.getFile() != null) {
+            fileName = fileDialog.getFile();
+            fileAddress = fileDialog.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileAddress + fileName)); //to fetch the adress in order to read the file
+            
+            gui.textArea.setText(" ");
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null ) {
+                gui.textArea.append( line + "\n");
+            }
+            bufferedReader.close();
+
+        } catch ( IOException e){
+            System.out.println("FILE NOT OPENED");
+        }
     }
 }
