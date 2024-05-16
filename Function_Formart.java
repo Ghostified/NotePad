132c0898111fc7dd71403f2b5f58311a0d1ffee1
Allan Branson <bransonaallan@gmail.com>
2024-05-16 16:12:29 +0300

    adding the Font Formart Jmenuitems

--- Function_Formart.java
+++ Function_Formart.java
@@ -1,8 +1,29 @@
 public class Function_Formart {
 
     GUI gui;
+    Font arial, comicSans, 
 
     public Function_Formart  (GUI gui) {
         this.gui = gui; 
+    }
+
+    public void wordWrap () {
+        if (gui.wordWrapOn == false) {
+            gui.wordWrapOn = true;
+            gui.textArea.setLineWrap(true);
+            gui.textArea.setWrapStyleWord(true);
+            gui.itemWrap.setText("Word Wrap: On");
+        }
+        else if (gui.wordWrapOn == true){
+            gui.wordWrapOn = false;
+            gui.textArea.setLineWrap(false);
+            gui.textArea.setWrapStyleWord(false);
+            gui.itemWrap.setText("Word Wrap: Off");
+
+        }
+    }
+
+    public void createFont () {
+
     }
 }
