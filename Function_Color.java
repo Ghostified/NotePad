2fcdd221958763ee4e3a38db8b342b762012cd01
Allan Branson <bransonaallan@gmail.com>
2024-05-20 14:45:00 +0300

    Completeing the background and foreground colors

--- Function_Color.java
+++ Function_Color.java
@@ -1,9 +1,26 @@
+import java.awt.*;
 public class Function_Color {
     GUI gui;
     public Function_Color (GUI gui) {
         this.gui= gui;
     }
     public void changeColor (String color) {
-        
+        switch (color ) {
+        case "Green" : 
+                gui.window.getContentPane().setBackground(Color.green);
+                gui.textArea.setBackground(Color.green);
+                gui.textArea.setForeground(Color.white);
+                break;
+        case "Blue" : 
+                gui.window.getContentPane().setBackground(Color.blue);
+                gui.textArea.setBackground(Color.blue);
+                gui.textArea.setForeground(Color.black);
+            break;
+        case "Red" : 
+            gui.window.getContentPane().setBackground(Color.red);
+            gui.textArea.setBackground(Color.red);
+            gui.textArea.setForeground(Color.white);
+            break;    
+        }
     }
 }
